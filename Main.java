8cb81ea4674a103e9de5fd0498be21b62a3164e4
AndreyS19 <125403674+AndreyS19@users.noreply.github.com>
2023-10-05 11:07:19 +0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,49 +1,6 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-import java.util.Scanner;
-public class Main {
+public class Main extends task {
     public static void main(String[] args) {
-
-        String glas="aeyuioаёуеыоэяиюAEIOUYАЕЁИОУЫЭЮЯ";
-        String sogl="qwrtpsdfghjklzxcvbnmйцкнгшщзхфвпрлджчсмтбBCDFGHJKLMNPQRSTVWXYZБВГДЖЗЙКЛМНПРСТФХЧЦШЩ";
-        Scanner scan = new Scanner(System. in );
-        String tex = scan.nextLine();
-        String[] sent=tex.split("\\.");
-        for (int i = 0; i < sent.length; i++) {
-            {
-                int vowcount = 0;
-                int conscount = 0;
-                for (int j = 0; j < sent[i].length(); j++) {
-                    {
-                        for (int k = 0; k < glas.length(); k++) {
-                            if (sent[i].charAt(j) == glas.charAt(k)) {
-                                vowcount++;
-                            }
-                            if (sent[i].charAt(j) == sogl.charAt(k)) {
-                                conscount++;
-                            }
-                        }
-                    }
-                }
-
-                if(vowcount>conscount)
-                {
-                    System.out.println("в "+(i+1)+" предложение больше гласных ("+vowcount+">"+conscount+")");
-                }
-                else if (vowcount<conscount)
-                {
-                    System.out.println("в "+(i+1)+" предложение больше согласных ("+conscount+">"+vowcount+")");
-                }
-                else if (vowcount==0 && conscount==0)
-                {
-                    System.out.println("в "+(i+1)+" предложении нет букв");
-                }
-                else
-                {
-                    System.out.println("в "+(i+1)+" предложение равное количество гласных и согласных ("+vowcount+")");
-                }
-
-            }
-        }
+        dosomemagic();
     }
+
 }
